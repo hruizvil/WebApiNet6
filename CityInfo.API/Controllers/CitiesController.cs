62b76c2b380ed8c6f0b124530c0250f4c213502a
Hugo Ruiz <hruizvil@uci.edu>
2022-05-04 22:27:07 -0700

    Add project files.

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController] // Helps to improve the development experience for web apis
+    public class CitiesController : ControllerBase
+    {
+        public JsonResult GetCities()
+        {
+            new JsonResult(
+                new List<object> {
+                    new {id = 1, name = "New York City"},
+                    new {id = 2, name = "Houston City"} 
+                });
+        }
+    }
+}
